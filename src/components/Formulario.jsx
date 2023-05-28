d34bef3a17ef60ea690424749204377328c71727
Luiz Gustavo da Silva <38591825+Luiz4us8av6@users.noreply.github.com>
2023-05-28 11:57:44 -0300

    feat: implementação do consumo da api

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -2,20 +2,61 @@ import { SearchOutlined } from "@ant-design/icons";
 import { useState } from "react";
 import WeatherData from "./WeatherData";
 
+const URL = import.meta.env.VITE_API;
+const apiKey = import.meta.env.VITE_API_KEY;
+
 function Formulario() {
+  const [search, setSearch] = useState("");
+  const [data, setData] = useState();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (!search) return;
+
+    getWeatherData(search);
+
+    setSearch("");
+  };
+
+  const getWeatherData = async (cidade) => {
+    const apiWeatherURL = `${URL}q=${cidade}&units=metric&appid=${apiKey}&lang=pt_br`;
+
+    const res = await fetch(apiWeatherURL);
+    const data = await res.json();
+
+    setData({
+      name: data.name,
+      temp: parseInt(data.main.temp),
+      desc: data.weather[0].description,
+      urlIconElement: `https://openweathermap.org/img/wn/${data.weather[0].icon}.png`,
+      urlIconCountry: `https://www.countryflagicons.com/FLAT/64/${data.sys.country}.png`,
+      umid: data.main.humidity,
+      wind: data.wind.speed,
+    });
+  };
+
   return (
     <>
       <div className="container">
         <div className="form">
-          <form action="">
+          <form onSubmit={handleSubmit}>
             <h3>Confira o clima da sua cidade:</h3>
             <div className="form-input-containers">
-              <input type="text" placeholder="Digite o nome da cidade" />
-              <button id="search"><SearchOutlined /></button>
+              <input
+                type="text"
+                placeholder="Digite o nome da cidade"
+                name="search"
+                onChange={(e) => setSearch(e.target.value)}
+                value={search}
+              />
+              <button id="search" type="submit">
+                <SearchOutlined />
+              </button>
             </div>
           </form>
         </div>
-        <WeatherData/>
+        {data && <WeatherData data={data} />}
       </div>
     </>
   );
--- src/components/WeatherData.jsx
+++ src/components/WeatherData.jsx
@@ -1,24 +1,29 @@
 import { CompassOutlined, CloudOutlined, CarOutlined } from "@ant-design/icons";
-import { useState, useEffect } from "react";
+import { useEffect } from "react";
 
-function WeatherData() {
-  var cidade = "Santa Cruz";
-  var temp = 59;
-  var desc = "Nublado";
-  var umid = 48;
-  var wind = 3;
+function WeatherData({ data }) {
+  const { name, temp, desc, urlIconCountry, urlIconElement, umid, wind } =
+    data;
+
+  useEffect(() => {
+  }, [data]);
+
   return (
     <>
       <div id="weather-data">
         <h2>
           <CompassOutlined />
-          <span>{cidade}</span>
-          <img src="https://www.countryflagicons.com/FLAT/64/BR.png"/>
+          <span>{name}</span>
+          <img src={urlIconCountry} alt="Bandeira do país" />
         </h2>
         <p id="temepatura">{temp} °C</p>
         <div id="description-container">
           <p id="description">{desc}</p>
-          <img src="https://openweathermap.org/img/wn/01d.png" alt="Condição do tempo" id="weather-icon" />
+          <img
+            src={urlIconElement}
+            alt="Condição do tempo"
+            id="weather-icon"
+          />
         </div>
         <div id="details-container">
           <p id="umidity">
